11cddbb87315ff38fefca933f31738d6d619198f
renon0730 <r.inoue@ibis-japan.co.jp>
2026-04-27 15:20:02 +0900

    room_usersテーブルの作成

--- src/main/java/in/tech_camp/chat_app/entity/RoomEntity.java
+++ src/main/java/in/tech_camp/chat_app/entity/RoomEntity.java
@@ -2,7 +2,10 @@ package in.tech_camp.chat_app.entity;
 
 import lombok.Data;
 
+import java.util.List;
+
 public class RoomEntity {
   private Integer id;
   private String name;
+  private List<RoomUserEntity> roomUsers;
 }
--- src/main/java/in/tech_camp/chat_app/entity/UserEntity.java
+++ src/main/java/in/tech_camp/chat_app/entity/UserEntity.java
@@ -2,10 +2,13 @@ package in.tech_camp.chat_app.entity;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserEntity {
   private Integer id;
   private String name;
   private String email;
   private String password;
+  private List<RoomUserEntity> roomUsers;
 }
