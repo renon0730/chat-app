afb04af2fe6beeb529c670e5f6b63816f4ba5c3c
renon0730 <r.inoue@ibis-japan.co.jp>
2026-04-27 11:38:45 +0900

    バリデーションの設定

--- src/main/java/in/tech_camp/chat_app/controller/UserController.java
+++ src/main/java/in/tech_camp/chat_app/controller/UserController.java
@@ -1,12 +1,19 @@
 package in.tech_camp.chat_app.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import in.tech_camp.chat_app.validation.ValidationOrder;
 
 import in.tech_camp.chat_app.entity.UserEntity;
 import in.tech_camp.chat_app.form.LoginForm;
@@ -31,7 +38,21 @@ public class UserController {
   }
 
   @PostMapping("/user")
-  public String createUser(@ModelAttribute("userForm") UserForm userForm, Model model) {
+  public String createUser(@ModelAttribute("userForm") @Validated(ValidationOrder.class) UserForm userForm, BindingResult result, Model model) {
+    userForm.validatePasswordConfirmation(result);
+    if (userRepository.existsByEmail(userForm.getEmail())) {
+      result.rejectValue("email", "null", "Email already exists");
+    }
+
+    if (result.hasErrors()) {
+      List<String> errorMessages = result.getAllErrors().stream()
+              .map(DefaultMessageSourceResolvable::getDefaultMessage)
+              .collect(Collectors.toList());
+
+      model.addAttribute("errorMessages", errorMessages);
+      model.addAttribute("userForm", userForm);
+      return "users/signUp";
+    }
     UserEntity userEntity = new UserEntity();
     userEntity.setName(userForm.getName());
     userEntity.setEmail(userForm.getEmail());
@@ -75,21 +96,19 @@ public class UserController {
     return "users/edit";
   }
 
-  @PostMapping("/users/{userId}")
-  public String updateUser(@PathVariable("userId") Integer userId, @ModelAttribute("user") UserEditForm userEditForm, Model model) {
-    UserEntity user = userRepository.findById(userId);
-    user.setName(userEditForm.getName());
-    user.setEmail(userEditForm.getEmail());
-
-    try {
-      userRepository.update(user);
-    } catch (Exception e) {
-      System.out.println("エラー：" + e);
+@PostMapping("/users/{userId}")
+  public String updateUser(@PathVariable("userId") Integer userId, @ModelAttribute("user") @Validated(ValidationOrder.class) UserEditForm userEditForm, BindingResult result, Model model) {
+    String newEmail = userEditForm.getEmail();
+    if (userRepository.existsByEmailExcludingCurrent(newEmail, userId)) {
+      result.rejectValue("email", "error.user", "Email already exists");
+    }
+    if (result.hasErrors()) {
+      List<String> errorMessages = result.getAllErrors().stream()
+                                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                    .collect(Collectors.toList());
+      model.addAttribute("errorMessages", errorMessages);
       model.addAttribute("user", userEditForm);
       return "users/edit";
     }
-  
-    return "redirect:/";
-   }
  }
 
