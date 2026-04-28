e39ffc04b746c11a82e0ffa6f0fb6183a1fd958e
renon0730 <r.inoue@ibis-japan.co.jp>
2026-04-28 09:44:32 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/entity/RoomEntity.java
+++ src/main/java/in/tech_camp/chat_app/entity/RoomEntity.java
@@ -1,9 +1,11 @@
 package in.tech_camp.chat_app.entity;
 
-import lombok.Data;
-
 import java.util.List;
 
+import lombok.Data;
+
+
+@Data
 public class RoomEntity {
   private Integer id;
   private String name;
--- src/main/java/in/tech_camp/chat_app/form/RoomForm.java
+++ src/main/java/in/tech_camp/chat_app/form/RoomForm.java
@@ -1,11 +1,14 @@
 package in.tech_camp.chat_app.form;
 
-import lombok.Data;
-
 import java.util.List;
+
+import in.tech_camp.chat_app.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Data
 public class RoomForm {
+   @NotBlank(message = "Room Name can't be blank",groups = ValidationPriority1.class)
   private String name;
   private List<Integer> memberIds;
 }
