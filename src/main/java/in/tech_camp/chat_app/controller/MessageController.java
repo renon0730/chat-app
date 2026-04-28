3177e18e0493ade0c518a26306c0e2bf5c2e9c50
renon0730 <r.inoue@ibis-japan.co.jp>
2026-04-28 14:28:24 +0900

    チャットルーム名の表示

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -45,7 +45,9 @@ public class MessageController {
     model.addAttribute("rooms", roomList);
 
     model.addAttribute("messageForm", new MessageForm());
-    model.addAttribute("roomId", roomId);
+
+    RoomEntity room = roomRepository.findById(roomId);
+    model.addAttribute("room", room);
 
     List<MessageEntity> messages = messageRepository.findByRoomId(roomId);
     model.addAttribute("messages", messages);
