5fc5a0a4bfaf429f36188abecc65e6176a4660c3
renon0730 <r.inoue@ibis-japan.co.jp>
2026-04-28 15:29:55 +0900

    チャットルームの削除機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
@@ -81,6 +82,12 @@ public class RoomController {
       }
     }
     
+    return "redirect:/";
+  }
+
+  @PostMapping("/rooms/{roomId}/delete")
+  public String deleteRoom(@PathVariable Integer roomId) {
+    roomRepository.deleteById(roomId);
     return "redirect:/";
   }
 }
